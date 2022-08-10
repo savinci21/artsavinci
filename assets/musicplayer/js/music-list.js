8d425c6599f4b58c2720761ecf9a23efaaf5e8cb
art-savinci <lowdeep1004@gamail.com>
2022-08-10 18:29:17 +0900

    first_blog

---
+++ assets/musicplayer/js/music-list.js
@@ -0,0 +1,161 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "A whiter shade of pale",  
+    artist: "Procol Harum",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "You Are Always On My Mind", 
+    artist: "Willy Nelson",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Moves Like Jagger",
+    artist: "Maroon5",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Englishman In New York",
+    artist: "Sting",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Love Yourself", 
+    artist: "Justin Bieber",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Love Hurts",  
+    artist: "Nazareth",
+    img: "music-6",
+    src: "music-6"
+  },
+
+{
+    name: "I Will Follow You", 
+    artist: "Ricky Nelson",
+    img: "music-7",
+    src: "music-7"
+  },
+  
+  {
+    name: "I Am Better Off", 
+    artist: "Wildson",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Let It Be Me",  
+    artist: "Phil Everly",
+    img: "music-9",
+    src: "music-9"
+  },
+
+  {
+    name: "Thunderclouds", 
+    artist: "Sia",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "If", 
+    artist: "Sissel",
+    img: "music-11",
+    src: "music-11"
+  },
+
+
+  {
+    name: "Stand by Me", 
+    artist: "Ben E.King",
+    img: "music-12",
+    src: "music-12"
+  },
+
+  {
+    name: "Wishes", 
+    artist: "Emi Fujita",
+    img: "music-13",
+    src: "music-13"
+  },
+
+  {
+    name: "Fall In love sometimes", 
+    artist: "Elton John",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Stay", 
+    artist: "M.W & Z",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Aubrey", 
+    artist: "Bread",
+    img: "music-16",
+    src: "music-16"
+  }, 
+  {
+    name: "Sisyphus", 
+    artist: "Andrew Bird",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Quando Quando", 
+    artist: "Michael Buble",
+    img: "music-17",
+    src: "music-18"
+  }, 
+  {
+    name: "Magic", 
+    artist: "Pilot",
+    img: "music-17",
+    src: "music-19"
+  }, 
+  {
+    name: "Under Pressure", 
+    artist: "Queen",
+    img: "music-17",
+    src: "music-20"
+  }, 
+  {
+    name: "Mo' Better Blues", 
+    artist: "Quartet",
+    img: "music-17",
+    src: "music-21"
+  }, 
+  {
+    name: "Long Long Time", 
+    artist: "Linda Ronstadt",
+    img: "music-17",
+    src: "music-22"
+  } 
+  
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
